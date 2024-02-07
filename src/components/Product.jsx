61a23fa5590a06a5dbf25c1681b916b352b501c0
Irinaaissa <irinaalisa7506@gmail.com>
2024-02-07 19:43:23 +0200

    nev

---
+++ src/components/Product.jsx
@@ -0,0 +1,18 @@
+// export const Product = props => {
+    // return (
+    //   <div>
+        {/* <h2>{props.name}</h2> */}
+              {/* <img src={props.imgUrl} alt={props.name} width="480" /> */}
+              {/* <p>Price: {props.price} credits</p> */}
+      {/* </div> */}
+    // );
+//   };
+  export const Product = ({ name, imgUrl, price }) => {
+    return (
+      <div>
+        <h2>{name}</h2>
+              <img src={imgUrl} alt={name} width="480" />
+              <p>Price: {price} credits</p>
+      </div>
+    );
+  };
